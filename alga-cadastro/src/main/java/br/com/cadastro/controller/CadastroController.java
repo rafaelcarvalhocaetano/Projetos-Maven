77c24701d86b6232f14711f7246b3173e7aa6984
Rafael Carvalho <rapha.pse@outlook.com>
2018-05-15 21:04:49 -0300

    Add App

--- alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
+++ alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
@@ -1,11 +1,18 @@
 package br.com.cadastro.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Observable;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.cadastro.model.StatusTitulos;
 import br.com.cadastro.model.Titulo;
 import br.com.cadastro.repository.TituloRepository;
 
@@ -18,16 +25,35 @@ public class CadastroController {
 	
 	@RequestMapping("/novo")
 	public ModelAndView index() {
+		
 		ModelAndView mav = new ModelAndView("view/CadastroTitulos");
+		//mav.addObject("todosStatusTitulos", StatusTitulos.values());
 		return mav;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String salvar(Titulo t) {
+	public ModelAndView salvar(Titulo t) {
 		tr.save(t);
 		//ModelAndView mav = new ModelAndView("view/CadastroTitulos");
 		System.out.println(">>>>> "+t.getDescricao());
-		return "view/CadastroTitulos";
+		ModelAndView mav = new ModelAndView("view/CadastroTitulos");
+		//mav.addObject("mensagem","Título salvo com sucesso");
+		return mav;
 	}
+	
+	@ModelAttribute("todosStatusTitulos")
+	public List<StatusTitulos> todosTitulos(){
+		return Arrays.asList(StatusTitulos.values());
+	}
+	
+	@RequestMapping()
+	public ModelAndView pesquisa(){
+		Iterable<Titulo> lt = tr.findAll();
+		ModelAndView mav = new ModelAndView("view/PesquisaTitulos");
+		mav.addObject("itens", lt);
+		return mav;
+	}
+	
+	//parei em produtividade  devtools e livereload 2.11
 
 }
