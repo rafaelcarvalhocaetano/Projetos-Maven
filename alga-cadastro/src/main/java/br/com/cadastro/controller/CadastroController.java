9f52799bc014ea2be7e8f7fcbe6a1580eb501375
Rafael Carvalho <rapha.pse@outlook.com>
2018-05-17 19:49:30 -0300

    Add App

--- alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
+++ alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
@@ -2,15 +2,16 @@ package br.com.cadastro.controller;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Observable;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cadastro.model.StatusTitulos;
 import br.com.cadastro.model.Titulo;
@@ -19,41 +20,47 @@ import br.com.cadastro.repository.TituloRepository;
 @Controller
 @RequestMapping("/titulos")
 public class CadastroController {
-	
+
 	@Autowired
 	private TituloRepository tr;
-	
+
 	@RequestMapping("/novo")
 	public ModelAndView index() {
-		
+
 		ModelAndView mav = new ModelAndView("view/CadastroTitulos");
-		//mav.addObject("todosStatusTitulos", StatusTitulos.values());
+		mav.addObject(new Titulo());
 		return mav;
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(Titulo t) {
-		tr.save(t);
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+
 		//ModelAndView mav = new ModelAndView("view/CadastroTitulos");
-		System.out.println(">>>>> "+t.getDescricao());
-		ModelAndView mav = new ModelAndView("view/CadastroTitulos");
+
+		if (errors.hasErrors()) {
+			return "view/CadastroTitulos";
+		}
+		tr.save(titulo);
+		//ModelAndView mav2 = new ModelAndView("redirect:/titulos/novo");
+		attributes.addFlashAttribute("mensagem","Título salvo com sucesso");
 		//mav.addObject("mensagem","Título salvo com sucesso");
-		return mav;
+		//return mav2;
+		return "redirect:/titulos/novo";
 	}
-	
+
 	@ModelAttribute("todosStatusTitulos")
-	public List<StatusTitulos> todosTitulos(){
+	public List<StatusTitulos> todosTitulos() {
 		return Arrays.asList(StatusTitulos.values());
 	}
-	
+
 	@RequestMapping()
-	public ModelAndView pesquisa(){
+	public ModelAndView pesquisa() {
 		Iterable<Titulo> lt = tr.findAll();
 		ModelAndView mav = new ModelAndView("view/PesquisaTitulos");
 		mav.addObject("itens", lt);
 		return mav;
 	}
-	
-	//parei em produtividade  devtools e livereload 2.11
+
+	// -->>>> 2.15
 
 }
--- alga-cadastro/src/main/java/br/com/cadastro/model/Titulo.java
+++ alga-cadastro/src/main/java/br/com/cadastro/model/Titulo.java
@@ -12,11 +12,14 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
-
-import ch.qos.logback.core.subst.Token.Type;
 
 @Entity
 @Table(name="cadastro_titulos")
@@ -26,12 +29,18 @@ public class Titulo {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message= "Descrição é um Campo obrigatório")
+	@Size(max=60, message="A descrição não pode conter mais de 60 caracteres.")
 	private String descricao;
 	
+	@NotNull(message="Data é obrigatória.")
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@DecimalMin(value="0.01", message="Valor não pode ser menor que 0,01")
+	@DecimalMax(value="9999999.99", message="Valor não pode ser 9.999.999,99")
+	@NotNull(message = "Valor é obrigatório")
 	@NumberFormat(pattern="#,##0.00")
 	private BigDecimal valor;
 	
