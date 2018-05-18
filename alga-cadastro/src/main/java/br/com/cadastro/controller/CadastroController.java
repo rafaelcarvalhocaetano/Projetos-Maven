e38ff07829304c40bd2dea4069c83e1c497de550
Rafael Carvalho <rapha.pse@outlook.com>
2018-05-18 19:32:44 -0300

    Add App

--- alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
+++ alga-cadastro/src/main/java/br/com/cadastro/controller/CadastroController.java
@@ -2,12 +2,14 @@ package br.com.cadastro.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -59,6 +61,13 @@ public class CadastroController {
 		ModelAndView mav = new ModelAndView("view/PesquisaTitulos");
 		mav.addObject("itens", lt);
 		return mav;
+	}
+	
+	@RequestMapping(value="{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView("view/CadastroTitulos"); 
+		mv.addObject(titulo);
+		return mv;
 	}
 
 	// -->>>> 2.15
